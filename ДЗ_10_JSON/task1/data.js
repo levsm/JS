e2b807c8d903f79964a873da6e8da0a1bf75e842
Lev <levsmirnov@mail.ru>
2023-02-08 20:13:03 +0200

    ДЗ_10_задача 1 и 2

---
+++ ДЗ_10_JSON/task1/data.js
@@ -0,0 +1,62 @@
+const dataInfo = `[
+    {
+        "id": "181913649",
+        "name": "Разноцветные пирожные.",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, exercitationem!",
+        "width": "500",
+        "height": "500",
+        "box_count": "2",
+        "captions": "0",
+        "url": "https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg"
+      },
+      {
+        "id": "87743020",
+        "name": "Розовые пончики",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, exercitationem!",
+        "width": "500",
+        "height": "500",
+        "box_count": "3",
+        "captions": "0",
+        "url": "https://bipbap.ru/wp-content/uploads/2018/06/backgrounds-delicius-donut-food-Favim.com-2183410.jpg"
+      },
+      {
+        "id": "112126428",
+        "name": "Клубника",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, exercitationem!",
+        "width": "500",
+        "height": "500",
+        "box_count": "3",
+        "captions": "0",
+        "url": "https://bipbap.ru/wp-content/uploads/2018/06/short.jpg"
+      },
+      {
+        "id": "181913649",
+        "name": "Разноцветные пирожные.",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, exercitationem!",
+        "width": "500",
+        "height": "500",
+        "box_count": "2",
+        "captions": "0",
+        "url": "https://bipbap.ru/wp-content/uploads/2018/06/3c980dd2e9c909ada7377cc89885231b.jpg"
+      },
+      {
+        "id": "87743020",
+        "name": "Розовые пончики",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, exercitationem!",
+        "width": "500",
+        "height": "500",
+        "box_count": "3",
+        "captions": "0",
+        "url": "https://bipbap.ru/wp-content/uploads/2018/06/backgrounds-delicius-donut-food-Favim.com-2183410.jpg"
+      },
+      {
+        "id": "112126428",
+        "name": "Клубника",
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, exercitationem!",
+        "width": "500",
+        "height": "500",
+        "box_count": "3",
+        "captions": "0",
+        "url": "https://bipbap.ru/wp-content/uploads/2018/06/short.jpg"
+      }
+]`
